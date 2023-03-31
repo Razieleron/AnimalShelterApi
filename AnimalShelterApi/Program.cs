23cb517bba003dbecb70278db04c807c406a740b
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-31 16:44:57 -0700

    add versioning information to AnimalsController.cs

--- AnimalShelterApi/Program.cs
+++ AnimalShelterApi/Program.cs
@@ -1,4 +1,5 @@
 using AnimalShelterApi.Models;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,16 @@ builder.Services.AddDbContext<AnimalShelterApiContext>(
                     )
                   )
                 );
+
+builder.Services.AddApiVersioning(opt =>
+                    {
+                        opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1,0);
+                        opt.AssumeDefaultVersionWhenUnspecified = true;
+                        opt.ReportApiVersions = true;
+                        opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                        new HeaderApiVersionReader("x-api-version"),
+                                                                        new MediaTypeApiVersionReader("x-api-version"));
+                    });
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
