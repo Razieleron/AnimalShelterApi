99e6b4a398d8750a662687bb39675cf63fa0f5d5
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-31 14:31:41 -0700

    researching versioning

--- AnimalShelterApi/Controllers/AnimalsController.cs
+++ AnimalShelterApi/Controllers/AnimalsController.cs
@@ -21,7 +21,7 @@ namespace AnimalShelterApi.Controllers
     {
       IQueryable<Animal> query = _db.Animals.AsQueryable();
 
-      if (species != null)
+      if (species != null) 
       {
         query = query.Where(entry => entry.Species == species);
       }
