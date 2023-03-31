09a2b27e9583334d6ee2b9f84acd001347a4ad21
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-03-31 13:03:36 -0700

    after lunch commit

--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -18,7 +18,7 @@ namespace AnimalShelterApi.Models
           new Animal { AnimalId = 2, Name = "Rexie", Species = "Dinosaur", Age = 10 },
           new Animal { AnimalId = 3, Name = "Matilda", Species = "Dinosaur", Age = 2 },
           new Animal { AnimalId = 4, Name = "Pip", Species = "Shark", Age = 4 },
-          new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Dinosaur", Age = 22 }
+          new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Dinosaur", Age = 22 } 
         ); 
     }
   }
